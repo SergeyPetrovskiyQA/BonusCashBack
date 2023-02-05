68cc3eb6e836c6b2e382072deff773915a8caaa8
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2023-02-05 22:13:31 +0300

    TestNG test

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void negativeTestRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainSecond() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(250);
+        int expected = 750;
+        Assert.assertEquals(actual, expected);
+    }
+}
